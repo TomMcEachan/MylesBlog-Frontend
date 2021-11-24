8f023e81d89410c63b6024332a08a45137b16d20
Thomas McEachan <Tom.a.mceachan@outlook.com>
2021-11-24 12:08:47 +0000

    added axios

--- worldofnabeeda-frontend/pages/index.js
+++ worldofnabeeda-frontend/pages/index.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
 
-
-export default function Home() {
+export default function Home({posts}) {
   return (
     <>
-    Home
+    <h1>{posts[0].title}</h1>
     </>
   )
 }
+
+
+export async function getStaticProps() {
+
+  const postsRes = await axios.get("http://localhost:8005/posts")
+
+  return {
+    props: {
+      posts: postsRes.data
+    }
+  }
+
+} 
