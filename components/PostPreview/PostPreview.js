3096b0ae5518761d2164c396781e0f27d4ad2a1c
Thomas McEachan <Tom.a.mceachan@outlook.com>
2021-12-14 11:17:41 +0000

    Added images to post preview

--- components/PostPreview/PostPreview.js
+++ components/PostPreview/PostPreview.js
@@ -1,17 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
 export default function PostPreview({post}) {
 
+        console.log(post.image);
+
         return (
             <>
                 <div className="postPreview">
+                    <img alt={post.image.caption} src={post.image[0].formats.medium.url} className="previewImage" />
                     <h3>{post.title}</h3>
                     <p>{post.description}</p>
                     <p className="category">{post.category}</p>
                     <Link href={`/posts/${post.id}`} passHref>  
-                    <button>Read This Blog</button>
+                    <button>Read More</button>
                     </Link>
                 </div>
             </>
