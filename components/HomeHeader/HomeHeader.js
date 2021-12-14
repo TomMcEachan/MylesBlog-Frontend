db6f4bac9496234c76092b834cd26170cab21eeb
Thomas McEachan <Tom.a.mceachan@outlook.com>
2021-12-14 17:52:11 +0000

    Added carousel to the homepage, awaiting content type from strapi

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Image from 'next/image';
+import HomeCarousel from '../HomeCarousel/HomeCarousel';
 
 export default function HomeHeader() {
     return (
         <header>
-            <h1>World of Nabeeda</h1>
-            <p>This blog is my journey through life – the adventures, the experiences, the lesson and the things I’m passionate about changing.</p>
+          <HomeCarousel />
         </header>
     )
 }
