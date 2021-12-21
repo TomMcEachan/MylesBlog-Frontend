deaef7f40c63d4be93ef4d2d0947737c0af437ad
Thomas McEachan <Tom.a.mceachan@outlook.com>
2021-12-21 11:58:30 +0000

    Chaged image sized to improve responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -15,11 +15,11 @@ export default function MyApp({ Component, pageProps, latestPosts}) {
   return (
   <div className="app">
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="../styles/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="../styles/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="../styles/favicon/favicon-16x16.png"/>
-      <link rel="manifest" href="../styles/favicon/site.webmanifest"/>
-      <link rel="mask-icon" href="../styles/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/styles/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/styles/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/styles/favicon/favicon-16x16.png"/>
+      <link rel="manifest" href="/styles/favicon/site.webmanifest"/>
+      <link rel="mask-icon" href="/styles/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
       <meta name="msapplication-TileColor" content="#da532c"/>
       <meta name="theme-color" content="#ffffff"/>
     </Head>
