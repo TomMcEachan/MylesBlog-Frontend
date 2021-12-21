3087e981b1cde399815d298915606dcb7571d877
Thomas McEachan <Tom.a.mceachan@outlook.com>
2021-12-21 12:37:53 +0000

    Added favicon to site

--- pages/_app.js
+++ pages/_app.js
@@ -15,13 +15,14 @@ export default function MyApp({ Component, pageProps, latestPosts}) {
   return (
   <div className="app">
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/styles/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/styles/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/styles/favicon/favicon-16x16.png"/>
-      <link rel="manifest" href="/styles/favicon/site.webmanifest"/>
-      <link rel="mask-icon" href="/styles/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="description" content="World of Nabeeda Blog" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />    
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />    
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />    
       <meta name="msapplication-TileColor" content="#da532c"/>
-      <meta name="theme-color" content="#ffffff"/>
+      <meta name="theme-color" content="#ffffff" />      
     </Head>
     <TopBar />
     <DesktopNav />
