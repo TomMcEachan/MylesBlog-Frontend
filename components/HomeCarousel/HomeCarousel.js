a9512d50df8f71c901a6db4feb1caaf813f5b46d
Thomas McEachan <38360055+TomMcEachan@users.noreply.github.com>
2022-02-03 17:32:07 +0000

    Develop (#37)
    * keywords and description info the post pages
    
    * Added <Head> tag to all current pages
    
    * Added titles to pages
    
    * Added code to programatically update carousel with data from strapi
    
    * Made the carousel full width
    
    * Created VideoPlayer Component
    
    * Changed blog routing to match title instead of id
    
    * Updated slug method
    
    * Updated home carousel
    
    * Began adding progress bar
    
    * Began work on progress bar
    
    * Removed progress bar
    
    * Fixed footer styling to centre content
    
    * changed footer colour to light pink
    
    * Updated NPM versions
    
    * began updating blog pages
    
    * updates
    
    * Updated blog pages
    
    * updates
    
    * added floating social sharing
    
    * Refactored code
    
    * Feature/category pages (#32)
    
    * Updates to content pages
    
    * continued updating travel posts page to build category page funtionality
    
    * Styled page preview components
    
    * Added content to all category pages
    
    * Updated favicons
    
    * updated content
    
    * Continued updating styles
    
    * updated footer styles
    
    * continued to update styles
    
    * Feature/category pages (#36)
    
    * Updates to content pages
    
    * continued updating travel posts page to build category page funtionality
    
    * Styled page preview components
    
    * Added content to all category pages
    
    * Updated favicons
    
    * updated content
    
    * Continued updating styles
    
    * updated footer styles
    
    * continued to update styles
    
    * Updated header colours
    
    * Added more pages
    
    * Updated styling
    
    * Updates
    
    * changed
    
    * Updated new pages

--- components/HomeCarousel/HomeCarousel.js
+++ components/HomeCarousel/HomeCarousel.js
@@ -24,7 +24,7 @@ export default function HomeCarousel({featuredPosts}) {
                 }}
                 activeIndicatorIconButtonProps={{
                     style: {
-                        backgroundColor: 'pink',
+                        backgroundColor: '#FDA300',
                         color: 'white'
                     }
                 }}
---
+++ pages/research.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function research() {
+  return <div></div>;
+}
