d121a2060d70c35d99fc2739280c8b48f860c049
Thomas McEachan <38360055+TomMcEachan@users.noreply.github.com>
2022-02-04 14:13:09 +0000

    Fixed bug with all age randomiser that randomised JSON (#44)

--- pages/all.js
+++ pages/all.js
@@ -2,21 +2,28 @@ import axios from 'axios';
 import Grid from '@mui/material/Grid';
 import Head from 'next/head';
 import PagePreview from '../components/PagePreview/PagePreview';
+import React, { useEffect } from 'react';
+
 
 export default function Posts({posts, title}) {
 
+  //This function shuffles the api request response
   function shufflePosts(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
   }
-    
-    return (
-     
-      <>
-      
-       {shufflePosts(posts)}
+
+  
+  //This function randomises the content upon load
+  function RenderAllContentOnce(posts, title){
+      useEffect(() => {
+        shufflePosts(posts);
+      });
+
+      return(
+        <>  
       <Head>
          <title>Myles McEachan - Blog | {title} </title>
        </Head>
@@ -33,11 +40,19 @@ export default function Posts({posts, title}) {
          </div>
        </main>
      </>
+      )
+  }
+
+
+    //This renders the randomised content 
+    return (
+      <>{RenderAllContentOnce(posts, title)}</>
 
     )
 }
 
 
+//This gets the content from the CMS
 export async function getStaticProps() {
 
     const postsRes = await axios.get("/posts")
